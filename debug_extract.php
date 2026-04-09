38159e350bf1b30632bbc3499e568304b061b37f
gregmelo <gregoryvericel6@gmail.com>
2026-04-09 11:31:08 +0200

    Remove obsolete validation and migration error logs for Doctrine

--- debug_extract.php
+++
@@ -1,9 +0,0 @@
-<?php
-$lines = file('var/log/dev.log');
-$output = [];
-foreach ($lines as $line) {
-    if (strpos($line, 'CRITICAL') !== false || strpos($line, 'Uncaught') !== false || strpos($line, 'exception') !== false) {
-        $output[] = $line;
-    }
-}
-file_put_contents('debug_exceptions.log', implode("", array_slice($output, -10)));
--- debug_request.php
+++
@@ -1,6 +0,0 @@
-<?php
-$_SERVER['REQUEST_URI'] = '/reservation/employee/reservations';
-$_SERVER['REQUEST_METHOD'] = 'GET';
-$_SERVER['REMOTE_ADDR'] = '127.0.0.1';
-$_SERVER['HTTP_HOST'] = 'localhost';
-require 'public/index.php';
